cdd63e327f1d36f645c5c2d9b78cdaee505337f4
dharanidharan1424 <leadsbydharan@gmail.com>
2025-12-25 09:23:15 +0530

    updated db credential

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "---",
-  user: "---",
-  password: "---",
-  database: "---",
+  host: "feminiq.in",
+  user: "Admin_Feminiqshop",
+  password: "Feminiq416$",
+  database: "feminiq",
   waitForConnections: true,
 });
 
