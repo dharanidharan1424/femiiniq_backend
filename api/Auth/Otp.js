01b5722d2b3be3a1172d7241252532bf988d1d4d
dharanidharan1424 <leadsbydharan@gmail.com>
2026-01-23 18:15:27 +0530

    fix login payload error

--- api/Auth/Otp.js
+++ api/Auth/Otp.js
@@ -9,11 +9,18 @@ const nodemailer = require("nodemailer");
 
 // SMTP Transporter for Artist Signup
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // STARTTLS
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  tls: {
+    rejectUnauthorized: false
+  },
+  connectionTimeout: 30000, // 30 seconds
+  greetingTimeout: 30000,
   debug: true,
   logger: true
 });
