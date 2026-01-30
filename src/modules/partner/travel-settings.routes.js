258ef3bf5041f545a9a8e37b866763543fb14376
dharanidharan1424 <leadsbydharan@gmail.com>
2026-01-30 18:19:08 +0530

    fix: correct authToken middleware path in travel-settings routes

--- src/modules/partner/travel-settings.routes.js
+++ src/modules/partner/travel-settings.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authenticateToken = require('../../middleware/authToken');
+const authenticateToken = require('../../../middleware/authToken');
 const { updateTravelSettings } = require('./travel-settings.controller');
 
 router.post('/', authenticateToken, updateTravelSettings);
